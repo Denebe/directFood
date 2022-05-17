b09560dcd7a83d500a794d4f12cc47eb1fbd85bc
denebe <twhjdfo01@daum.net>
2022-05-17 12:23:27 +0900

    Fixed untraked file

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const mainApi = async() => {
 
-//http://openapi.foodsafetykorea.go.kr/api/cf6f5c54e1ec4c2b8e8f/서비스명/json/1/5
+//http://openapi.foodsafetykorea.go.kr/api/apikey/서비스명/json/1/5
 
   return instance({
     url: `/${API_KEY}/I2810/json/1/5`,
