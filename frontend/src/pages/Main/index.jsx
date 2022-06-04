ebef461b5febe41d18bc05c91150faf0341f0b77
abc9455 <dldudwls888@gmail.com>
2022-06-05 01:03:32 +0900

    [style] 메인 페이지 디자인

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react'
 import * as Styled from './styled'
-import Background from '../../img/background.jpg';
 import {mainApi} from '../../api/Api'
 
 const Main = () => {
@@ -10,9 +9,15 @@ const Main = () => {
     },[db])
     return(
         <Styled.FlexWrapper>
-            <Styled.MainText>BLOCK<Styled.StyledBr />FOOD</Styled.MainText>
             <Styled.BackgroundWrapper>
-                <Styled.BackgroundImg src={Background}/>
+                <Styled.StyledLogo src='logo2.png' />
+                <Styled.Icon src='virus3.png' />
+                <Styled.MainText>BLOCK<Styled.StyledBr />FOOD</Styled.MainText>
+                <Styled.MainText id='sub'>위해 식품 차단 정보 사이트</Styled.MainText>
+                <Styled.Icon src='circle.png' id='circle' />
+                <Styled.MainText id='bottom'>위해식품이란 식품, 식품첨가물, 기구 또는 용기ㆍ포장에 존재하는 위험요소로서 <Styled.StyledBr />
+                인체의 건강을 해치거나 해칠 우려가 있는 것을 말한다.</Styled.MainText>
+                <Styled.Icon src='scrolldown.png' id='scroll' />
             </Styled.BackgroundWrapper>
         </Styled.FlexWrapper>
     )
--- frontend/src/pages/Main/styled.js
+++ frontend/src/pages/Main/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Background from '../../img/background.jpg';
 
 export const FlexWrapper = styled.div`
     display: flex;
@@ -8,15 +9,39 @@ export const FlexWrapper = styled.div`
 `
 
 export const BackgroundWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-image: url(${Background});
+    background-size: cover;
+    width: 1400px;
+    height: 920px;
+`
+
+export const StyledLogo = styled.img`
+    position: absolute;
+    width: 100px;
+    top: 5%;
+    left: 8%;
+    cursor: pointer;
+`
+
+export const Icon = styled.img`
+    color: #fff;
+    width: 55px;
+    &#circle {
+        width: 16px;
+        margin-top: 70px;
+    }
+    &#scroll {
+        position: absolute;
+        bottom: -20%;
+        width: 42px;
+    }
 `
 
 export const StyledBr = styled.br`
-`
-
-export const BackgroundImg = styled.img`
-    position: absolute;
-    right: 0%;
-    bottom: -45%;
 `
 
 export const MainText = styled.div`
@@ -24,7 +49,23 @@ export const MainText = styled.div`
     font-weight: 700;
     font-size: 60px;
     text-align: center;
+    margin-top: 38px;
     line-height: 65px;
-    color: #eee;
+    letter-spacing: 3px;
+    color: #fff;
     z-index: 90;
+    &#sub {
+        font-size: 16px;
+        margin-top: 10px;
+        letter-spacing: 0.9px;
+        line-height: 36px;
+    }
+    &#bottom {
+        position: absolute;
+        bottom: -10%;
+        font-family: NotoSansKR;
+        font-size: 16px;
+        line-height: 18px;
+        letter-spacing: 0.8px;
+    }
 `
