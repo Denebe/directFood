1a05d789ffeefcc81f5727ca804bab011254fd63
abc9455 <dldudwls888@gmail.com>
2022-06-18 18:06:06 +0900

    [feat] footerBar 디자인 업데이트

--- frontend/src/Components/Footer/index.jsx
+++ frontend/src/Components/Footer/index.jsx
@@ -3,7 +3,11 @@ import * as Styled from "./styled"
 const FooterBar = () => {
     return (
         <Styled.Wrapper>
-            
+            <div style={{display:"flex", alignItems:"center"}}>
+                <Styled.FooterText>Block Food</Styled.FooterText>
+                <Styled.FooterText id="copyright">ⓒ blockfood. All rights reserved.</Styled.FooterText>
+            </div>
+            <img src="logo2.png" width={70} />
         </Styled.Wrapper>
     )
 }
